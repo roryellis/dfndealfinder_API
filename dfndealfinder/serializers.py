023e8ce661ca4e55a80f94982d9ed3c31990f562
roryellis <rdellis1@gmail.com>
2020-06-10 17:13:40 -0500

    Added query filtering to specialslist view

--- dfndealfinder/serializers.py
+++ dfndealfinder/serializers.py
@@ -3,22 +3,20 @@ from .models import Restaurant, Special, CuisineType, DiningType, SpecialCategor
 
 
 class SpecialSerializer(serializers.ModelSerializer):
-    restaurant = serializers.HyperlinkedRelatedField(
-        view_name='restaurant_detail', read_only=True)
+    restaurant = serializers.StringRelatedField()
+
+    restaurant_url = serializers.ModelSerializer.serializer_url_field(
+        view_name='restaurant_detail')
 
     category = serializers.StringRelatedField(many=True)
-
+    
     class Meta:
         model = Special
-        fields = ('id', 'restaurant', 'title', 'description', 'promo_image', 'category', 'daily_special', 'monday', 'tuesday',
+        fields = ('id', 'restaurant', 'restaurant_url', 'title', 'description', 'promo_image', 'category', 'daily_special', 'monday', 'tuesday',
                   'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'limited_time', 'start_date', 'end_date',)
 
+
 class RestaurantSerializer(serializers.ModelSerializer):
-    # specials = serializers.HyperlinkedIdentityField(
-    #     view_name='special_detail',
-    #     many=True,
-    #     read_only=True
-    # )
 
     specials = SpecialSerializer(many=True)
 
@@ -34,29 +32,3 @@ class RestaurantSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'address', 'city', 'state', 'zip', 'dining_options', 'cuisine',
                   'description', 'restaurant_image', 'website_url', 'specials', 'restaurant_url')
         lookup_field = 'name'
-
-
-
-
-
-# class DiningTypeSerializer(serializers.ModelSerializer):
-
-#     class Meta:
-#         model = DiningType
-#         fields = ('id', 'name')
-
-
-# # class DiningTypeSerializer(serializers.ModelSerializer):
-# #     restaurants = serializers.HyperlinkedRelatedField(
-# #         view_name='restaurant_detail', read_only=True)
-
-# #     class Meta:
-# #         model = DiningType
-# #         fields = ('id', 'name', 'restaurants')
-
-
-# class CuisineTypeSerializer(serializers.ModelSerializer):
-
-#     class Meta:
-#         model = DiningType
-#         fields = ('id', 'name')
--- dfndealfinder/views.py
+++ dfndealfinder/views.py
@@ -13,8 +13,49 @@ class RestaurantDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = RestaurantSerializer
 
 class SpecialList(generics.ListCreateAPIView):
-    queryset = Special.objects.all()
+    # queryset = Special.objects.all()
     serializer_class = SpecialSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = Special.objects.all()
+        category = self.request.query_params.get('category', None)
+        daily_special = self.request.query_params.get('daily_special', None)
+        monday = self.request.query_params.get('monday', None)
+        tuesday = self.request.query_params.get('tuesday', None)
+        wednesday = self.request.query_params.get('wednesday', None)
+        thursday = self.request.query_params.get('thursday', None)
+        friday = self.request.query_params.get('friday', None)
+        saturday = self.request.query_params.get('saturday', None)
+        sunday = self.request.query_params.get('sunday', None)
+        limited_time = self.request.query_params.get('limited_time', None)
+        start_date = self.request.query_params.get('start_date', None)
+        end_date = self.request.query_params.get('end_date', None)
+
+        if category is not None:
+            queryset = queryset.filter(category__name__icontains=category)
+        if daily_special is not None:
+            queryset = queryset.filter(daily_special=daily_special)
+        if monday is not None:
+            queryset = queryset.filter(monday=monday)
+        if tuesday is not None:
+            queryset = queryset.filter(tuesday=tuesday)
+        if wednesday is not None:
+            queryset = queryset.filter(wednesday=wednesday)
+        if thursday is not None:
+            queryset = queryset.filter(thursday=thursday)
+        if friday is not None:
+            queryset = queryset.filter(friday=friday)
+        if saturday is not None:
+            queryset = queryset.filter(saturday=saturday)
+        if sunday is not None:
+            queryset = queryset.filter(sunday=sunday)
+        if limited_time is not None:
+            queryset = queryset.filter(limited_time=limited_time)
+        if start_date is not None:
+            queryset = queryset.filter(start_date__lte=start_date)
+        if end_date is not None:
+            queryset = queryset.filter(end_date__gte=end_date)
+        return queryset
 
 
 class SpecialDetail(generics.RetrieveUpdateDestroyAPIView):
